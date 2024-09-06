bd7f709c83c9d7b87316eb81edd88116f0ce23c1
Leonardo Blume <leribeiro.blume@gmail.com>
2024-09-06 11:23:41 -0300

    commit inicial

---
+++ src/main/java/br/com/adocao_pet/SistemaDeAdocaoParaPetApplication.java
@@ -0,0 +1,13 @@
+package br.com.adocao_pet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaDeAdocaoParaPetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaDeAdocaoParaPetApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/adocao_pet/SistemaDeAdocaoParaPetApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.adocao_pet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaDeAdocaoParaPetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
