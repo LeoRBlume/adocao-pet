9047b4ee1957e74d7c347393c94bb620e81b639b
Leonardo Blume <leribeiro.blume@gmail.com>
2024-09-08 09:32:36 -0300

    refactor: removing logs from BreedSpecification

--- src/main/java/br/com/adocao_pet/infrastructure/persistence/repository/specification/BreedSpecification.java
+++ src/main/java/br/com/adocao_pet/infrastructure/persistence/repository/specification/BreedSpecification.java
@@ -4,8 +4,6 @@ import br.com.adocao_pet.infrastructure.persistence.entity.BreedEntity;
 import jakarta.persistence.criteria.CriteriaBuilder;
 import jakarta.persistence.criteria.CriteriaQuery;
 import jakarta.persistence.criteria.Root;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.data.jpa.domain.Specification;
 
 import java.time.LocalDate;
@@ -13,16 +11,15 @@ import java.time.LocalDateTime;
 import java.time.LocalTime;
 
 public class BreedSpecification {
-    private static final Logger logger = LoggerFactory.getLogger(BreedSpecification.class);
 
+    private BreedSpecification() {
+    }
 
     public static Specification<BreedEntity> hasName(String name) {
         return (Root<BreedEntity> root, CriteriaQuery<?> query, CriteriaBuilder criteriaBuilder) -> {
             if (name == null || name.isEmpty()) {
-                logger.debug("Filtering by name: no filter applied.");
                 return criteriaBuilder.conjunction();
             }
-            logger.debug("Filtering by name: {}", name);
             return criteriaBuilder.equal(root.get("name"), name);
         };
     }
@@ -30,13 +27,11 @@ public class BreedSpecification {
     public static Specification<BreedEntity> hasCreationDate(LocalDate creationDate) {
         return (Root<BreedEntity> root, CriteriaQuery<?> query, CriteriaBuilder criteriaBuilder) -> {
             if (creationDate == null) {
-                logger.debug("Filtering by creationDate: no filter applied.");
                 return criteriaBuilder.conjunction();
             }
             LocalDateTime startOfDay = creationDate.atStartOfDay();
             LocalDateTime endOfDay = creationDate.atTime(LocalTime.MAX);
 
-            logger.debug("Filtering by creationDate: between {} and {}", startOfDay, endOfDay);
             return criteriaBuilder.between(root.get("date"), startOfDay, endOfDay);
         };
     }
